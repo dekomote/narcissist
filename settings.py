1caf6e608fea25be553ed6d5ebf66be0b9961c0c
Dejan Noveski <dr.mote@gmail.com>
2011-04-05 18:51:10 +0000

    In simple theme, menu moved to the left

--- settings.py
+++ settings.py
@@ -9,6 +9,9 @@ THEME = "simple"
 SERVICES = (
             service("page", name = "index", title = "Bio", 
                 extra = {"file": "index.html"}),
+
+            service("page", name = "about", title = "About Me", 
+                extra = {"file": ""}),
             
             service("twitter", name = "dekomote", title = "@dekomote", 
                 extra = {"username": "dekomote"},),
